44b6f1514355ad975e38e190ed42f4884013e181
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2023-03-20 17:37:30 +0530

    for beta uses

--- src/context/firebase.jsx
+++ src/context/firebase.jsx
@@ -14,23 +14,23 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage'
 
 const firebaseContext = createContext(null);
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDWcOmSGhs7rjRh1u0nkjfnF4Iwd07kX0E",
-//     authDomain: "instagram-87c95.firebaseapp.com",
-//     projectId: "instagram-87c95",
-//     storageBucket: "instagram-87c95.appspot.com",
-//     messagingSenderId: "864345540422",
-//     appId: "1:864345540422:web:0006de6d8ccb6378878747"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyC9K6IqaXISk7EJ8xsEvVr99_viiEi--Lg",
-    authDomain: "instagram-beta-f4791.firebaseapp.com",
-    projectId: "instagram-beta-f4791",
-    storageBucket: "instagram-beta-f4791.appspot.com",
-    messagingSenderId: "504052711345",
-    appId: "1:504052711345:web:4a37a041d9dc72ec61c790"
+    apiKey: "AIzaSyDWcOmSGhs7rjRh1u0nkjfnF4Iwd07kX0E",
+    authDomain: "instagram-87c95.firebaseapp.com",
+    projectId: "instagram-87c95",
+    storageBucket: "instagram-87c95.appspot.com",
+    messagingSenderId: "864345540422",
+    appId: "1:864345540422:web:0006de6d8ccb6378878747"
 };
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC9K6IqaXISk7EJ8xsEvVr99_viiEi--Lg",
+//     authDomain: "instagram-beta-f4791.firebaseapp.com",
+//     projectId: "instagram-beta-f4791",
+//     storageBucket: "instagram-beta-f4791.appspot.com",
+//     messagingSenderId: "504052711345",
+//     appId: "1:504052711345:web:4a37a041d9dc72ec61c790"
+// };
 
 const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase)

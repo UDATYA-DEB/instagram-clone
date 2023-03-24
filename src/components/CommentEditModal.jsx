5961e2c39b7eb9c0eb73a19b44e6cb20ba1d8fa4
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2023-03-25 03:41:17 +0530

    report button added

--- src/components/CommentEditModal.jsx
+++ src/components/CommentEditModal.jsx
@@ -37,7 +37,7 @@ const CommentEditModal = ({setOpen, commentPath, userEmail, open}) => {
           <CloseIcon className='closeicon' onClick={handleClose}/>
           <div className='post-modal'>
             <div>
-              {firebaseContext.currentUser.email === userEmail && <Button onClick={handleDelete} variant="primary" >Delete Comment</Button>}
+              {firebaseContext.currentUser.email === userEmail ? <Button onClick={handleDelete} variant="primary" >Delete Comment</Button> : <Button onClick={()=>alert('Dummy Reported')} variant="danger" >Report Comment</Button>}
             </div>
             {firebaseContext.currentUser.email === firebaseContext.adminEmail && <Button onClick={handleDelete} variant="danger" style={{marginTop: '10px'}}>Admin Delete Comment</Button>}
           </div>

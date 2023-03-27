e494a8d29c7a042d2ef1d09f565db6945b443d07
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2023-03-27 17:49:05 +0530

    delete post for other users fixed.

--- src/components/PostMenuModal.jsx
+++ src/components/PostMenuModal.jsx
@@ -38,10 +38,10 @@ const PostMenuModal = ({setOpen, imageURL, postNum, userEmail, open}) => {
           <CloseIcon className='closeicon' onClick={handleClose}/>
           <div className='post-modal'>
             <div>
-              {firebaseContext.currentUser.email === userEmail && deletePost ? <Button style={{opacity: '0.6', cursor: 'default'}} variant="primary" >Deleting post...</Button> : <Button onClick={handleDelete} variant="primary" >Delete post</Button>}
+              {firebaseContext.currentUser.email === userEmail && <Button onClick={handleDelete} style={{marginBottom: '10px'}} variant="primary" >{deletePost ? 'Deleting...' : 'Delete Post'}</Button>}
             </div>
-            {firebaseContext.currentUser.email === firebaseContext.adminEmail && <Button onClick={handleDelete} variant="danger" style={{marginTop: '10px'}}>Admin Delete post</Button>}
-            <Button variant="primary" style={{marginTop: '10px'}}>Repost</Button>
+            {firebaseContext.currentUser.email === firebaseContext.adminEmail && <Button onClick={handleDelete} variant="danger" style={{marginBottom: '10px'}}>{deletePost ? 'Deleting...' : 'Admin Delete Post'}</Button>}
+            <Button variant="primary" style={{marginBottom: '10px'}}>Repost</Button>
           </div>
         </div>
         </Fade>

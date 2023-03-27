df92d1049343422b719dfd0d364426a924141490
Udatya Deb <74970338+UDATYA-DEB@users.noreply.github.com>
2023-03-27 19:08:01 +0530

    comment edit modal style fixed padding

--- src/components/CommentEditModal.jsx
+++ src/components/CommentEditModal.jsx
@@ -37,9 +37,9 @@ const CommentEditModal = ({setOpen, commentPath, userEmail, open}) => {
           <CloseIcon className='closeicon' onClick={handleClose}/>
           <div className='post-modal'>
             <div>
-              {firebaseContext.currentUser.email === userEmail ? <Button onClick={handleDelete} variant="primary" >Delete Comment</Button> : <Button onClick={()=>alert('Dummy Reported')} variant="danger" >Report Comment</Button>}
+              {firebaseContext.currentUser.email === userEmail ? <Button style={{marginBottom: '10px'}} onClick={handleDelete} variant="primary" >Delete Comment</Button> : <Button style={{marginBottom: '10px'}} onClick={()=>alert('Dummy Reported')} variant="danger" >Report Comment</Button>}
             </div>
-            {firebaseContext.currentUser.email === firebaseContext.adminEmail && <Button onClick={handleDelete} variant="danger" style={{marginTop: '10px'}}>Admin Delete Comment</Button>}
+            {firebaseContext.currentUser.email === firebaseContext.adminEmail && <Button onClick={handleDelete} variant="danger" style={{marginBottom: '10px'}}>Admin Delete Comment</Button>}
           </div>
         </div>
         </Fade>
